256bd677a053b95f9d2f9bf7319bdb255fdc6cfc
Kryštof Novosad <novosadkry@seznam.cz>
2025-10-07 11:23:36 +0200

    feat: add client to aspire

--- UTB-AP5PW-Invoicer.AppHost/AppHost.cs
+++ UTB-AP5PW-Invoicer.AppHost/AppHost.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.UTB_AP5PW_Invoicer_Server>("utb-ap5pw-invoicer-server");
+var server = builder.AddProject<Projects.UTB_AP5PW_Invoicer_Server>("utb-ap5pw-invoicer-server")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("utb-ap5pw-invoicer-client", "../UTB-AP5PW-Invoicer.Client")
+    .WithReference(server)
+    .WaitFor(server)
+    .WithEnvironment("BROWSER", "none")
+    .WithHttpEndpoint(env: "VITE_PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
