c8d7d3cfb7481aba6182d864dcef1fe48524c7dd
Kryštof Novosad <novosadkry@seznam.cz>
2025-12-01 17:37:17 +0100

    chore: add migration to remove vat pricing

--- UTB-AP5PW-Invoicer.Infrastructure/Data/Migrations/20251201163642_RemoveVat.Designer.cs
+++ UTB-AP5PW-Invoicer.Infrastructure/Data/Migrations/20251201163642_RemoveVat.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UTB_AP5PW_Invoicer.Infrastructure.Data;
@@ -11,9 +12,11 @@ using UTB_AP5PW_Invoicer.Infrastructure.Data;
 namespace UTB_AP5PW_Invoicer.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251201163642_RemoveVat")]
+    partial class RemoveVat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UTB-AP5PW-Invoicer.Infrastructure/Data/Migrations/20251201163642_RemoveVat.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UTB_AP5PW_Invoicer.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveVat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalVat",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "TotalVat",
+                table: "InvoiceItems");
+
+            migrationBuilder.DropColumn(
+                name: "VatRate",
+                table: "InvoiceItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalVat",
+                table: "Invoices",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalVat",
+                table: "InvoiceItems",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "VatRate",
+                table: "InvoiceItems",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
