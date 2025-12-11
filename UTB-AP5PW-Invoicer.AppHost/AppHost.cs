dce3270fad20bb7b9e744896d9ef165b2e4330b0
Kryštof Novosad <novosadkry@seznam.cz>
2025-12-11 17:30:34 +0100

    chore: support for container deployment

--- UTB-AP5PW-Invoicer.AppHost/AppHost.cs
+++ UTB-AP5PW-Invoicer.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var database = builder.AddPostgres("utb-ap5pw-invoicer-db")
@@ -10,13 +12,26 @@ var server = builder.AddProject<Projects.UTB_AP5PW_Invoicer_Server>("utb-ap5pw-i
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
-builder.AddNpmApp("utb-ap5pw-invoicer-client", "../UTB-AP5PW-Invoicer.Client")
-    .WithReference(server)
-    .WaitFor(server)
-    .WithEnvironment("BROWSER", "none")
-    .WithEnvironment("VITE_API_URL", server.GetEndpoint("https"))
-    .WithHttpEndpoint(env: "VITE_PORT")
-    .WithExternalHttpEndpoints()
-    .PublishAsDockerFile();
+if (builder.Environment.IsDevelopment())
+{
+    builder.AddNpmApp("utb-ap5pw-invoicer-client", "../UTB-AP5PW-Invoicer.Client")
+        .WithReference(server)
+        .WaitFor(server)
+        .WithEnvironment("BROWSER", "none")
+        .WithEnvironment("VITE_API_URL", server.GetEndpoint("https"))
+        .WithHttpEndpoint(env: "VITE_PORT")
+        .WithExternalHttpEndpoints()
+        .PublishAsDockerFile();
+}
+else
+{
+    builder.AddDockerfile("utb-ap5pw-invoicer-client", "../UTB-AP5PW-Invoicer.Client")
+        .WithReference(server)
+        .WaitFor(server)
+        .WithEnvironment("BROWSER", "none")
+        .WithEnvironment("VITE_API_URL", server.GetEndpoint("https"))
+        .WithHttpEndpoint(env: "VITE_PORT", targetPort: 80)
+        .WithExternalHttpEndpoints();
+}
 
 builder.Build().Run();
